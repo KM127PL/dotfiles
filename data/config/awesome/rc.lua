ac42b5cef6f5b78cb853d61d39e487f155aeaee2
KM127PL <km127pl2@gmail.com>
2022-10-15 18:40:42 +0200

    Update rc.lua

--- data/config/awesome/rc.lua
+++ data/config/awesome/rc.lua
@@ -332,17 +332,6 @@ globalkeys = gears.table.join(
     -- Prompt
     awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
               {description = "run prompt", group = "launcher"}),
-
-    awful.key({ modkey }, "x",
-              function ()
-                  awful.prompt.run {
-                    prompt       = "Run: ",
-                    textbox      = awful.screen.focused().mypromptbox.widget,
-                    exe_callback = os.execute,
-                    history_path = awful.util.get_cache_dir() .. "/history_eval"
-                  }
-              end,
-              {description = "lua execute prompt", group = "awesome"}),
     -- Menubar
     awful.key({ modkey }, "p", function() menubar.show() end,
               {description = "show the menubar", group = "launcher"})
